3ad6b64c4752889af6a25f20993d980f3f493e75
eric vz <evonzee@themxgroup.com>
2022-04-28 10:20:16 -0500

    use role name, not arn, to attach policy

--- autoscaling.tf
+++ autoscaling.tf
@@ -2,6 +2,6 @@ module "autoscaling" {
   count = var.autoscaling.enabled ? 1 : 0
   source = "./autoscaling"
   cluster_name = local.cluster_name
-  granted_roles = { for name, group in module.kubernetes.eks_managed_node_groups : name => group.iam_role_arn }
+  granted_roles = { for name, group in module.kubernetes.eks_managed_node_groups : name => group.iam_role_name }
   autoscaling_version = var.autoscaling.version
 }

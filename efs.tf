bfcfd4345d7ae78481d925757570224515e65791
eric vz <evonzee@themxgroup.com>
2022-04-28 10:18:59 -0500

    don't deploy efs unless there's two nodes

--- efs.tf
+++ efs.tf
@@ -1,5 +1,5 @@
 module "efs" {
-  count = var.enable_efs ? 1 : 0
+  count = (var.enable_efs && var.autoscaling_min >= 2) ? 1 : 0
   source = "./efs"
   cluster_name = local.cluster_name
   region_code = data.aws_region.current.name

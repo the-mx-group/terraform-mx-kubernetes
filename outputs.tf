0191fb828f8f8382f270a70f8be8c18d686dcf46
eric vz <evonzee@themxgroup.com>
2022-05-12 14:59:21 -0500

    add oidc arn output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,10 @@ output "vpc_cidr" {
   value = data.aws_vpc.kubernetes.cidr_block
   description = "The ID of the security group created for nodes"
 }
+output "oidc_arn" {
+  value = module.kubernetes.oidc_provider_arn
+  description = "ARN for the OIDC provider for this cluster"
+}
 output "security_group" {
   value = aws_security_group.kubernetes.id
   description = "The ID of the security group created for nodes"

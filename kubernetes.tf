4d79a0446ab05c4e6a574e96eb2ff6e2cbdec60e
eric vz <evonzee@themxgroup.com>
2022-09-23 18:30:22 -0500

    update kubernetes module and vpc cni versions

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,13 +1,13 @@
 #actually provision the kubernetes cluster
 module "kubernetes" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.26.6"
+  version         = "18.29.0"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   cluster_addons = merge(
     {
       "vpc-cni" : {
-        "addon_version" : "v1.11.0-eksbuild.1",
+        "addon_version" : "v1.11.3-eksbuild.1",
         "resolve_conflicts" : "OVERWRITE"
       }
     },

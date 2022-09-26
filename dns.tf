4bcc3fa663a97b5007f03e2261deabdb7c3ccf99
eric vz <evonzee@themxgroup.com>
2022-09-26 17:22:32 -0500

    update to latest lablabs external dns

--- dns.tf
+++ dns.tf
@@ -1,9 +1,9 @@
 module "eks-external-dns" {
   source  = "lablabs/eks-external-dns/aws"
-  version = "0.9.0"
+  version = "1.1.0"
 
   cluster_identity_oidc_issuer     = module.kubernetes.oidc_provider
   cluster_identity_oidc_issuer_arn = module.kubernetes.oidc_provider_arn
-  k8s_irsa_role_name_prefix        = local.cluster_name
+  irsa_role_name_prefix            = local.cluster_name
   helm_chart_version               = local.external_dns_helm_version
 }

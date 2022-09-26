3be3a041a09bbc56a05053f067869b36ea584c7d
eric vz <evonzee@themxgroup.com>
2022-09-26 17:28:11 -0500

    update to latest external dns and ingress charts

--- locals.tf
+++ locals.tf
@@ -5,12 +5,12 @@ locals {
   create_vpc        = var.vpc_id == ""
 
   // ingress
-  ingress_helm_version     = "1.4.1"
-  ingress_manifest_version = "2.4.1" // should reference whatever the helm chart points at
+  ingress_helm_version     = "1.4.5" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
+  ingress_manifest_version = "2.4.4" // should reference whatever the helm chart points at
 
   // metrics
   metrics_version = "3.8.2"
 
   // external dns
-  external_dns_helm_version = "6.2.4" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
+  external_dns_helm_version = "6.10.0" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
 }

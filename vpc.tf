7fa501b6fb626258b241080f4a3bb802d5506eb8
eric vz <evonzee@themxgroup.com>
2022-10-21 11:48:25 -0500

    add an ipv6 block to generated VPCs

--- vpc.tf
+++ vpc.tf
@@ -7,6 +7,7 @@ resource "aws_vpc" "kubernetes" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
+  assign_generated_ipv6_cidr_block = true
 
   tags = {
     Name       = "${local.friendly_name} VPC"

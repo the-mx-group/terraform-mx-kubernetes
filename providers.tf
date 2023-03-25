6dc166ff72eee7776782e17e64dec29e9fd12763
eric vz <evonzee@themxgroup.com>
2023-03-25 15:37:12 -0500

    remove old kubectl provider

--- providers.tf
+++ providers.tf
@@ -12,13 +12,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.kubernetes.token
 }
 
-# used to patch the aws-auth configmap
-provider "kubectl" { # remove this once everyone has upgraded to the latest version
-  host                   = module.kubernetes.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.kubernetes.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.kubernetes.token
-}
-
 provider "helm" {
   kubernetes {
     host                   = module.kubernetes.cluster_endpoint
--- versions.tf
+++ versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1"
 
   required_providers {
-    kubectl = { # remove this provider eventually
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.10"

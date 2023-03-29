03e2dd37ffcca8d58a3d49742a6d3c53edf671df
eric vz <evonzee@themxgroup.com>
2023-03-29 09:08:48 -0500

    update external dependencies

--- locals.tf
+++ locals.tf
@@ -5,12 +5,12 @@ locals {
   create_vpc        = var.vpc_id == ""
 
   // ingress
-  ingress_helm_version     = "1.4.5" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
-  ingress_manifest_version = "2.4.4" // should reference whatever the helm chart points at
+  ingress_helm_version     = "1.4.8" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
+  ingress_manifest_version = "2.4.7" // should reference whatever the helm chart points at
 
   // metrics
-  metrics_version = "3.8.2"
+  metrics_version = "3.9.0"
 
   // external dns
-  external_dns_helm_version = "6.10.0" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
+  external_dns_helm_version = "6.15.0" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
 }

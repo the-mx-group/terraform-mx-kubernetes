978e24c411bd9b346f2931526af90d09eacd3553
eric vz <evonzee@themxgroup.com>
2023-05-16 14:07:54 -0500

    change some prometheus rules to align with services available in aws eks

--- prometheus/locals.tf
+++ prometheus/locals.tf
@@ -2,5 +2,13 @@ locals {
   prometheus_stack_version = "45.28.0"
   prometheus_helm_values = {
     "kubernetesServiceMonitors.enabled" = false,
+    "kubeControllerManager.enabled" = false,
+    "defaultRules.rules.kubeControllerManager" = false,
+    "kubeProxy.enabled" = false,
+    "defaultRules.rules.kubeProxy" = false,
+    "kubelet.enabled" = false,
+    "defaultRules.rules.kubelet" = false,
+    "defaultRules.rules.kubeSchedulerAlerting" = false,
+    "defaultRules.rules.kubeSchedulerRecording" = false,
   }
 }

30be9e68a175a0f56c8177bcf1eaffd2d357b77b
eric vz <evonzee@themxgroup.com>
2023-06-21 10:33:30 -0500

    update efs permissions policy

--- efs/main.tf
+++ efs/main.tf
@@ -59,39 +59,53 @@ resource "aws_iam_policy" "efs-policy" {
   # Terraform expression result to valid JSON syntax.
   policy = <<EOF
 {
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Effect": "Allow",
-			"Action": [
-				"elasticfilesystem:DescribeAccessPoints",
-				"elasticfilesystem:DescribeFileSystems"
-			],
-			"Resource": "*"
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"elasticfilesystem:CreateAccessPoint"
-			],
-			"Resource": "*",
-			"Condition": {
-				"StringLike": {
-					"aws:RequestTag/efs.csi.aws.com/cluster": "true"
-				}
-			}
-		},
-		{
-			"Effect": "Allow",
-			"Action": "elasticfilesystem:DeleteAccessPoint",
-			"Resource": "*",
-			"Condition": {
-				"StringEquals": {
-					"aws:ResourceTag/efs.csi.aws.com/cluster": "true"
-				}
-			}
-		}
-	]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:DescribeAccessPoints",
+        "elasticfilesystem:DescribeFileSystems",
+        "elasticfilesystem:DescribeMountTargets",
+        "ec2:DescribeAvailabilityZones"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:CreateAccessPoint"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:RequestTag/efs.csi.aws.com/cluster": "true"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:TagResource"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:ResourceTag/efs.csi.aws.com/cluster": "true"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": "elasticfilesystem:DeleteAccessPoint",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "aws:ResourceTag/efs.csi.aws.com/cluster": "true"
+        }
+      }
+    }
+  ]
 }
 EOF
 }

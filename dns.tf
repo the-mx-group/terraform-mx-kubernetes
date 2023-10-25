69551ff7dccd05f0dba7f34af405b593e1d32322
eric vz <evonzee@themxgroup.com>
2023-10-25 14:50:12 -0500

    update module versions

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 module "eks-external-dns" {
   source  = "lablabs/eks-external-dns/aws"
-  version = "1.1.1"
+  version = "1.2.0"
 
   cluster_identity_oidc_issuer     = module.kubernetes.oidc_provider
   cluster_identity_oidc_issuer_arn = module.kubernetes.oidc_provider_arn
--- locals.tf
+++ locals.tf
@@ -5,13 +5,13 @@ locals {
   create_vpc        = var.vpc_id == ""
 
   // ingress
-  ingress_helm_version     = "1.5.2" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
-  ingress_manifest_version = "2.5.1" // should reference whatever the helm chart points at
+  ingress_helm_version     = "1.6.1" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
+  ingress_manifest_version = "2.6.1" // should reference whatever the helm chart points at
 
   // metrics
-  metrics_version = "3.10.0"   // latest helm chart from https://github.com/kubernetes-sigs/metrics-server/releases
+  metrics_version = "3.11.0"   // latest helm chart from https://github.com/kubernetes-sigs/metrics-server/releases
 
   // external dns
-  external_dns_helm_version = "6.20.1" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
+  external_dns_helm_version = "6.27.0" // uses https://artifacthub.io/packages/helm/bitnami/external-dns
 
 }

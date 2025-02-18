c4e80a9e136547e756e5c384e359eebd5f74557d
eric vz <evonzee@themxgroup.com>
2025-02-18 14:11:10 -0600

    update to use ephemeral resources, which requires tf 1.10

--- providers.tf
+++ providers.tf
@@ -1,9 +1,4 @@
-data "aws_eks_cluster" "kubernetes" {
-  name       = module.kubernetes.cluster_name
-  depends_on = [module.kubernetes]
-}
-
-data "aws_eks_cluster_auth" "kubernetes" {
+ephemeral "aws_eks_cluster_auth" "kubernetes" {
   name       = module.kubernetes.cluster_name
   depends_on = [module.kubernetes]
 }
@@ -11,13 +6,13 @@ data "aws_eks_cluster_auth" "kubernetes" {
 provider "kubernetes" {
   host                   = module.kubernetes.cluster_endpoint
   cluster_ca_certificate = base64decode(module.kubernetes.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.kubernetes.token
+  token                  = ephemeral.aws_eks_cluster_auth.kubernetes.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.kubernetes.cluster_endpoint
     cluster_ca_certificate = base64decode(module.kubernetes.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.kubernetes.token
+    token                  = ephemeral.aws_eks_cluster_auth.kubernetes.token
   }
 }

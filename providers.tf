d9a1dca16c0f0329bb720f85945d85eb22807f23
eric vz <evonzee@themxgroup.com>
2025-02-18 14:08:08 -0600

    use latest version of terraform eks

--- providers.tf
+++ providers.tf
@@ -1,17 +1,17 @@
 data "aws_eks_cluster" "kubernetes" {
-  name = module.kubernetes.cluster_name
-  depends_on = [ module.kubernetes ]
+  name       = module.kubernetes.cluster_name
+  depends_on = [module.kubernetes]
 }
 
 data "aws_eks_cluster_auth" "kubernetes" {
-  name = module.kubernetes.cluster_name
-  depends_on = [ module.kubernetes ]
+  name       = module.kubernetes.cluster_name
+  depends_on = [module.kubernetes]
 }
 
 provider "kubernetes" {
   host                   = module.kubernetes.cluster_endpoint
   cluster_ca_certificate = base64decode(module.kubernetes.cluster_certificate_authority_data)
-  token = data.aws_eks_cluster_auth.kubernetes.token
+  token                  = data.aws_eks_cluster_auth.kubernetes.token
 }
 
 provider "helm" {

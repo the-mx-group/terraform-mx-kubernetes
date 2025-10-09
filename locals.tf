9002afb11f2344a8986994360d8c05b85ce72297
eric vz <evonzee@themxgroup.com>
2025-10-09 17:18:59 -0500

    update ingress, prometheus versions

--- locals.tf
+++ locals.tf
@@ -6,13 +6,13 @@ locals {
   create_nat_gateway = length(var.private_subnets.networks) > 0 && var.private_subnets.nat_gateway.gateway_id == null
 
   // ingress
-  ingress_helm_version     = "1.9.1" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
-  ingress_manifest_version = "2.9.1" // should reference whatever the helm chart points at
+  ingress_helm_version     = "1.14.0" // https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
+  ingress_manifest_version = "2.14.0" // should reference whatever the helm chart points at
 
   // metrics
   metrics_version = "3.12.2"   // latest helm chart from https://github.com/kubernetes-sigs/metrics-server/releases
 
   // prometheus
-  prometheus_stack_version = "65.2.0" // https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack/
+  prometheus_stack_version = "78.0.0" // https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack/
 
 }
